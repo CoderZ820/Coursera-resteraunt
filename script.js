36a552777c546f7761493cf5e47efd126660e25a
Mark Deebach <markdeebach@gmail.com>
2022-10-08 16:33:36 -0500

    Fixed navbar, added JS and html

---
+++ script.js
@@ -0,0 +1,24 @@
+$(function () {
+    $("#navbarToggle").blur(function (event) {
+        var screenWidth = window.innerWidth;
+        if (screenWidth < 768) {
+            $("#collapsable-nav").collapse('hide');
+        }
+    });
+});
+
+(function (global) {
+    var dc = {};
+    var homeHtml = "snippets/home-snippet.html";
+    var insertHtml = function (selector, html) {
+        var targetElem = document.querySelector(selector);
+        targetElem.innerHTML = html;
+    };
+
+    var showLoading = function (selector) {
+        var html = "<div class='text-center'>";
+        html += "<img src='images/ajax-loader.gif'></div>";
+        insertHtml(selector, html);
+    };
+
+})(window);
